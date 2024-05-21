088bbcda8e697dadd2c9c794975054c2900afa97
Gabrielesuma <gabrielesuma139@gmail.com>
2024-05-21 17:02:20 +0200

    title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,12 @@
 @section('content')
 <main>
     <h1>Home page</h1>
+    <ul>
+        @foreach ($movies as $movie)
+            <li>{{$movie->title}}</li>
+        @endforeach
+    </ul>
+    
 </main>
 
 @endsection
